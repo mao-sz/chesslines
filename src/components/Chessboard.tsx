698eb46c617cbe7b0e30132a2db41cf1c53aa908
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2025-03-14 23:43:19 +0000

    Display notification upon incorrect move

--- src/components/Chessboard.tsx
+++ src/components/Chessboard.tsx
@@ -11,17 +11,19 @@ const RANK = [8, 7, 6, 5, 4, 3, 2, 1, 0];
 const FILE = 'abcdefgh';
 
 export function Chessboard({ line, playerColour }: ChessboardProps) {
-    // TODO: Handle success responses
     const { position, playMove, success } = useChess(line, playerColour);
     const [fromSquare, setFromSquare] = useState<string | null>(null);
+    const [shouldShowFeedback, setShouldShowFeedback] = useState(false);
+
     const displayPosition = playerColour === 'w' ? position : reverse(position);
 
     function handleSquareClick(e: MouseEvent): void {
-        const square = e.currentTarget as HTMLDivElement;
+        const square = e.currentTarget as HTMLElement;
         const { rank, file, contains } = square.dataset;
 
         // clicking empty square but not moving piece
         if (!fromSquare && !contains) {
+            setShouldShowFeedback(false);
             return;
         }
 
@@ -32,38 +34,48 @@ export function Chessboard({ line, playerColour }: ChessboardProps) {
 
         if (!fromSquare || isOwnPiece) {
             setFromSquare(`${file}${rank}`);
+            setShouldShowFeedback(false);
         } else {
             playMove({ from: fromSquare, to: `${file}${rank}` });
             setFromSquare(null);
+            setShouldShowFeedback(true);
         }
     }
 
     function clearMove(e: MouseEvent): void {
         e.preventDefault();
         setFromSquare(null);
+        setShouldShowFeedback(false);
     }
 
     return (
-        <div className={styles.board}>
-            {displayPosition.split('/').map((row, rank) => (
-                <div key={rank}>
-                    {expandEmptySquares(row).map((square, file) => (
-                        <Square
-                            key={file}
-                            contains={square}
-                            rank={playerColour === 'w' ? RANK[rank] : rank + 1}
-                            file={
-                                playerColour === 'w'
-                                    ? FILE[file]
-                                    : reverse(FILE)[file]
-                            }
-                            selectedSquare={fromSquare}
-                            registerSquare={handleSquareClick}
-                            clearMove={clearMove}
-                        />
-                    ))}
-                </div>
-            ))}
-        </div>
+        <>
+            <div className={styles.board}>
+                {displayPosition.split('/').map((row, rank) => (
+                    <div key={rank}>
+                        {expandEmptySquares(row).map((square, file) => (
+                            <Square
+                                key={file}
+                                contains={square}
+                                rank={
+                                    playerColour === 'w' ? RANK[rank] : rank + 1
+                                }
+                                file={
+                                    playerColour === 'w'
+                                        ? FILE[file]
+                                        : reverse(FILE)[file]
+                                }
+                                selectedSquare={fromSquare}
+                                registerSquare={handleSquareClick}
+                                clearMove={clearMove}
+                            />
+                        ))}
+                    </div>
+                ))}
+            </div>
+            {!success && shouldShowFeedback && (
+                <p className={styles.incorrect}>Incorrect</p>
+            )}
+        </>
     );
 }
