2ab5ae2476d3232a780ad504e3f6b2318b3dea80
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2025-03-17 00:36:26 +0000

    Load navigable shuffled lines in Trainer component

--- src/pages/Trainer.tsx
+++ src/pages/Trainer.tsx
@@ -1,25 +1,36 @@
 import { useState } from 'react';
 import { Chessboard } from '../components/Chessboard';
-import { useChess } from '../helpers/hooks';
-import { Line } from '../types';
+import { useChess, useShuffledLines } from '../helpers/hooks';
+import type { Line } from '../types';
 import styles from './trainer.module.css';
 
 export function Trainer() {
-    // TODO: Remove after testing
-    const pgn = '1. e4 e5 2. Nc3 Nf6 3. f4 exf4 4. e5';
-    const playerColour = 'w';
+    // TODO: Make a Trainer prop later
+    const lines: Line[] = [
+        { pgn: '1. e4 e5 2. Nc3 Nf6 3. f4 exf4 4. e5 Qe7', player: 'w' },
+        { pgn: '1. e4 e5 2. Nc3 Nf6', player: 'b' },
+        { pgn: '1. e4 e5 2. Nc3 Nf6', player: 'w' },
+        { pgn: '1. e4 e6', player: 'w' },
+        { pgn: '1. e4 a5 2. Nf3 Nf6', player: 'b' },
+        { pgn: '1. d4 d5 2. Nc3 Nf6', player: 'w' },
+    ];
 
+    const { currentLine, toNextLine, progress } = useShuffledLines(lines);
     const { position, playMove, moveSuccess, lineSuccess } = useChess(
-        pgn,
-        playerColour
+        currentLine,
+        progress
     );
     const [shouldShowFeedback, setShouldShowFeedback] = useState(false);
 
+    const hasLinesLeft = progress < lines.length;
+
+    // TODO: Style me!
     return (
         <>
+            {hasLinesLeft && <button onClick={toNextLine}>Next</button>}
             <Chessboard
                 position={position}
-                playerColour={playerColour}
+                playerColour={currentLine.player}
                 playMove={playMove}
                 setShouldShowFeedback={setShouldShowFeedback}
             />
@@ -28,6 +39,7 @@ export function Trainer() {
                 <p className={styles.incorrect}>Incorrect</p>
             )}
             {lineSuccess && <p>Well done!</p>}
+            {`${progress}/${lines.length}`}
         </>
     );
 }
