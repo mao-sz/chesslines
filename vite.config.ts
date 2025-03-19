aca91b12cc72ad8d2f76b472c90927b14c4ce98e
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2025-03-19 17:05:47 +0000

    Set up testing environment

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [tsconfigPaths(), react()],
-    test: { environment: 'jsdom', setupFiles: './tests/setup.ts' },
+    test: { environment: 'jsdom', setupFiles: './src/testing/setup.ts' },
 });
