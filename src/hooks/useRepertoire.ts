6fe2f8690bc1371c71861155f37b127525f4f98e
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2025-03-21 23:52:35 +0000

    Create useRepertoire custom hook

---
+++ src/hooks/useRepertoire.ts
@@ -0,0 +1,55 @@
+import type { RepertoireFolders, RepertoireLines } from '@/types/types';
+import { useState } from 'react';
+
+export function useRepertoire() {
+    const emptyRepertoire: RepertoireFolders = {
+        w: { name: 'White', contains: 'either', children: [] },
+        b: { name: 'Black', contains: 'either', children: [] },
+    };
+    const [folders, setFolders] = useState<RepertoireFolders>(emptyRepertoire);
+    const [lines, setLines] = useState<RepertoireLines>({});
+
+    function addFolder({
+        name,
+        parent,
+    }: {
+        name: string;
+        parent: keyof RepertoireFolders;
+    }) {
+        const newFolderUUID = crypto.randomUUID();
+        const newFolder = { name: name, contains: 'either', children: [] };
+        const newParent = {
+            ...folders[parent],
+            contains: 'folders',
+            children: [...folders[parent].children, newFolderUUID],
+        };
+
+        setFolders({
+            ...folders,
+            [parent]: newParent,
+            [newFolderUUID]: newFolder,
+        });
+    }
+
+    function addLine({
+        startingFEN,
+        pgn,
+        parent,
+    }: {
+        startingFEN: string;
+        pgn: string;
+        parent: keyof RepertoireFolders;
+    }) {
+        const newLineUUID = crypto.randomUUID();
+        const newParentFolder = {
+            ...folders[parent],
+            contains: 'lines',
+            children: [...folders[parent].children, newLineUUID],
+        };
+
+        setLines({ ...lines, [newLineUUID]: { startingFEN, pgn } });
+        setFolders({ ...folders, [parent]: newParentFolder });
+    }
+
+    return { folders, lines, addFolder, addLine };
+}
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,8 +1,28 @@
 import type { Dispatch, SetStateAction } from 'react';
 
+type NonEmptyArray<T> = [T, ...T[]];
+
 export type Colour = 'w' | 'b';
 export type FENPosition = string & {};
 export type MoveInfo = { from: string; to: string };
 export type StateSetter<T> = Dispatch<SetStateAction<T>>;
 export type Line = { pgn: string; player: Colour };
 export type PixelCoordinates = { x: number; y: number };
+
+type UUID = `${string}-${string}-${string}-${string}-${string}`;
+
+type RepertoireLine = { startingFEN: string; pgn: string };
+export type RepertoireLines = Record<UUID, RepertoireLine>;
+
+type Folder = { name: string };
+type EmptyFolder = Folder & { contains: 'either'; children: [] };
+type NonEmptyFolder = Folder & {
+    contains: 'folders' | 'lines';
+    children: NonEmptyArray<UUID>;
+};
+type RepertoireFolder = EmptyFolder | NonEmptyFolder;
+export type RepertoireFolders = {
+    w: RepertoireFolder & { name: 'White' };
+    b: RepertoireFolder & { name: 'Black' };
+    [key: UUID]: RepertoireFolder;
+};
