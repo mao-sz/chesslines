6ff13c23da552f9ffbc3c546ae90d8cd99c03e5b
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2025-07-15 03:15:22 +0100

    Enable custom-media @-rule syntax via postCSS

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import postcssCustomMedia from 'postcss-custom-media';
+import postcssGlobalData from '@csstools/postcss-global-data';
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [tsconfigPaths(), react()],
+    css: {
+        postcss: {
+            plugins: [
+                postcssGlobalData({ files: ['./src/app/reset.css'] }),
+                postcssCustomMedia(),
+            ],
+        },
+    },
     test: {
         environment: 'jsdom',
         setupFiles: './src/testing/setup.ts',
