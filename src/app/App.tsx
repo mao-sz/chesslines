67ca34f6a1a635f465c4c6b33fa7f263b3fa011e
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2025-08-06 21:35:35 +0100

    Amend inappropriate global styles
    Some styles causing issues because they had no business being part of a
    global base rule to compose into other local rules. Those styles should
    be placed in the local rules that need them.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
 import { Outlet } from 'react-router';
 import { Header } from './Header';
+import { RepertoireErrorPage } from '@/features/repertoireErrors/RepertoireErrorPage';
 import { LOCAL_STORAGE } from '@/util/localStorage';
 import { EMPTY_REPERTOIRE } from '@/util/constants';
 import type { OutletContext } from '@/types/utility';
-import { RepertoireErrorPage } from '@/features/repertoireErrors/RepertoireErrorPage';
 
 export function App() {
     const { validationError, repertoire } = LOCAL_STORAGE.repertoire.get();
